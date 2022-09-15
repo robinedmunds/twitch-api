45c403eedd08801b5875e9760e125d01aa4de893
Robin Edmunds <8656189+robinedmunds@users.noreply.github.com>
2022-09-15 23:44:52 +0100

    Move login var to global constant var

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from twitch_api import TwitchApi
 
 load_dotenv()
 app = Flask(__name__, template_folder="templates")
+LOGIN = "anarchicuk"
 
 twitch = TwitchApi(**{
     "client_id": os.environ["CLIENT_ID"],
@@ -33,26 +34,23 @@ def auth():
 
 @app.route("/user", methods=["GET"])
 def user():
-    login = "anarchicuk"
     if twitch.token_is_valid() is True:
-        return twitch.fetch_user(login)
+        return twitch.fetch_user(LOGIN)
     return "no auth token in memory"
 
 
 @app.route("/follows", methods=["GET"])
 def follows():
-    login = "anarchicuk"
     if twitch.token_is_valid() is True:
-        res = twitch.fetch_follows(login)
+        res = twitch.fetch_follows(LOGIN)
         if res:
             return res
-        return "error contacting api, login: {}".format(login)
+        return "error contacting api, login: {}".format(LOGIN)
     return "no auth token in memory"
 
 
 @app.route("/write", methods=["GET"])
 def write():
-    login = "anarchicuk"
     if twitch.token_is_valid() is True:
-        return twitch.write_ps1_files(login)
+        return twitch.write_ps1_files(LOGIN)
     return "no auth token in memory"
